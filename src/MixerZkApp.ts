4c6bf4a0275205e8d1850f98a444656be9007af6
Juan Pablo Garzón <jgarzonebrath08@gmail.com>
2022-10-09 04:54:54 -0500

    Verifier SM implemented and insertCommitment method developed

--- src/MixerZkApp.ts
+++ src/MixerZkApp.ts
@@ -22,10 +22,51 @@ const doProofs = true;
 
 await isReady;
 
+type Witness = { isLeft: boolean; sibling: Field }[];
+
+const MerkleTreeHeight = 8;
+
+/** Merkle Tree
+ * Instance for global reference. It must be stored off-chain.
+ */
+const MerkleTree = Experimental.MerkleTree;
+const merkleTree = new MerkleTree(MerkleTreeHeight);
+
+class MerkleWitness extends Experimental.MerkleWitness(MerkleTreeHeight) {}
+
+export class Verifier extends SmartContract {
+  @state(Field) x = State<Field>();
+
+  @method update(y: Field) {
+    this.emitEvent('update', y);
+    let x = this.x.get();
+    this.x.assertEquals(x);
+    let newX = x.add(y);
+    this.x.set(newX);
+    // return newX;
+  }
+
+  deploy(args: DeployArgs) {
+    super.deploy(args);
+    this.setPermissions({
+      ...Permissions.default(),
+      editState: Permissions.proofOrSignature(),
+      send: Permissions.proofOrSignature(),
+    });
+    this.balance.addInPlace(UInt64.fromNumber(initialBalance));
+    this.x.set(initialState);
+  }
+
+  @method verifyProof(merkleProof: MerkleWitness) {
+    //
+  }
+}
+
 export class MixerZkApp extends SmartContract {
   @state(Field) x = State<Field>();
-  //Commitment state variable
-  @state(Field) commitment = State<Field>();
+  @state(Field) merkleTreeRoot = State<Field>();
+  @state(Field) lastIndexAdded = State<Field>();
+
   events = {
     update: Field,
     payout: UInt64,
@@ -40,7 +81,10 @@ export class MixerZkApp extends SmartContract {
       send: Permissions.proofOrSignature(),
     });
     this.balance.addInPlace(UInt64.fromNumber(initialBalance));
+
     this.x.set(initialState);
+    this.merkleTreeRoot.set(merkleTree.getRoot());
+    this.lastIndexAdded.set(new Field(0));
   }
 
   @method update(y: Field) {
@@ -52,32 +96,22 @@ export class MixerZkApp extends SmartContract {
     // return newX;
   }
 
-  // /**
-  //  * This method allows a certain privileged account to claim half of the zkapp balance, but only once
-  //  * @param caller the privileged account
-  //  */
-  // @method payout(caller: PrivateKey) {
-  //   // check that caller is the privileged account
-  //   let callerAddress = caller.toPublicKey();
-  //   // callerAddress.assertEquals(privilegedAddress);
+  @method insertCommitment(commitment: Field) {
+    let lastIndex = this.lastIndexAdded.get();
+    let valor = toStr(lastIndex);
+    // console.log("Internal --------------------");
+    // console.log("this.root --> ", this.merkleTreeRoot.get());
+    // let indexForNextCommitment = this.lastIndexAdded.get().toString() + 1n;
+    // merkleTree.setLeaf(indexForNextCommitment, commitment);
 
-  //   // assert that the caller account is new - this way, payout can only happen once
-  //   let callerAccountUpdate = AccountUpdate.defaultAccountUpdate(callerAddress);
-  //   callerAccountUpdate.account.isNew.assertEquals(Bool(true));
+    // let newMerkleTreeRoot = merkleTree.getRoot();
+    // this.merkleTreeRoot.set(newMerkleTreeRoot);
+    // console.log("this.root --> ", this.merkleTreeRoot.get());
+  }
+}
 
-  //   // pay out half of the zkapp balance to the caller
-  //   let balance = this.account.balance.get();
-  //   this.account.balance.assertEquals(balance);
-  //   // FIXME UInt64.div() doesn't work on variables
-  //   let halfBalance = Circuit.witness(UInt64, () =>
-  //     balance.toConstant().div(2)
-  //   );
-  //   this.send({ to: callerAccountUpdate, amount: halfBalance });
-
-  //   // emit some events
-  //   this.emitEvent('payoutReceiver', callerAddress);
-  //   this.emitEvent('payout', halfBalance);
-  // }
+async function toStr(value: Field) {
+  return value.toString();
 }
 
 let Local = Mina.LocalBlockchain();
@@ -102,6 +136,7 @@ let initialBalance = 10_000_000_000;
 
 // let initialBalance2 = 10_000;
 let initialState = Field(1);
+console.log('ZkAppAddress: ', zkappAddress);
 let zkapp = new MixerZkApp(zkappAddress);
 
 if (doProofs) {
@@ -126,28 +161,28 @@ console.log('Deploy');
 /**
  * 1. A Harpo account that will pay the gas feeds is funded
  */
-// let tx = await Mina.transaction(harpoFeePayer, () => {
-//   AccountUpdate.fundNewAccount(harpoFeePayer, { initialBalance });
-//   //One time deploy
-//   zkapp.deploy({ zkappKey });
-//   console.log('ACCOUNTS USER ');
-//   console.log(userAccountAddress);
-// });
-// await tx.send();
-// console.log('HarpoWallet funded succesfully');
+let tx = await Mina.transaction(harpoFeePayer, () => {
+  AccountUpdate.fundNewAccount(harpoFeePayer, { initialBalance });
+  //One time deploy
+  zkapp.deploy({ zkappKey });
+  console.log('ACCOUNTS USER ');
+  console.log(userAccountAddress);
+});
+await tx.send();
+console.log('HarpoWallet funded succesfully');
 
 /**
  * 2. A userAccount is  funded with the purpose of depositing into our harpoAccount.
  * Note: Will not happen in a real implementation
  */
 
-// let tx2 = await Mina.transaction(harpoFeePayer, () => {
-//   AccountUpdate.fundNewAccount(harpoFeePayer);
-//   let update = AccountUpdate.createSigned(harpoFeePayer);
-//   //The userAddress is funced
-//   update.send({ to: userAccountAddress, amount: 10 });
-//   console.log('Funding Harpo Wallet');
-// });
+let tx2 = await Mina.transaction(harpoFeePayer, () => {
+  AccountUpdate.fundNewAccount(harpoFeePayer);
+  let update = AccountUpdate.createSigned(harpoFeePayer);
+  //The userAddress is funced
+  update.send({ to: userAccountAddress, amount: 20 });
+  console.log('Funding Harpo Wallet');
+});
 
 //Sending transaction
 /**
@@ -160,67 +195,65 @@ console.log('Deploy');
  * Rho: Private key
  */
 
-// async function createNullifier(publicKey: PublicKey) {
-//   let keyString = publicKey.toFields();
-//   let secretField = Field.random();
-//   let nullifierHash = Poseidon.hash([...keyString, secretField]);
+async function createNullifier(publicKey: PublicKey) {
+  let keyString = publicKey.toFields();
+  let secretField = Field.random();
+  let nullifierHash = Poseidon.hash([...keyString, secretField]);
 
-//   // let nullifierField= new Field(nullifier
-//   return nullifierHash;
-// }
+  return nullifierHash;
+}
 console.log('Second TX');
-// await tx2.send();
+await tx2.send();
 console.log('UserWallet funded succesfully');
 // console.log('initial state: ' + zkapp.x.get());
-// let accountsHAarpo = zkapp.account;
-// let nullifier = await createNullifier(userAccountAddress);
-// let commitment = await createCommitment(nullifier);
-// console.log('User PB: ' + JSON.stringify(userAccountAddress));
-// console.log('User PK: ' + userAccountKey);
-// console.log(`User balance: ${Mina.getBalance(userAccountAddress)} MINA`);
-// console.log(
-//   `Harpo Account Balance: ${Mina.getBalance(harpoFeePayerAccount)} MINA`
-// );
-// console.log(`initial balance: ${zkapp.account.balance.get().div(1e9)} MINA`);
-// console.log(`Nullifier ` + nullifier);
-// console.log(`Commitment  ` + commitment);
+let accountsHarpo = zkapp.account;
+let nullifier = await createNullifier(userAccountAddress);
+let commitment = await createCommitment(nullifier);
+console.log('User PB: ' + JSON.stringify(userAccountAddress));
+console.log('User PK: ' + userAccountKey);
+console.log(`User balance: ${Mina.getBalance(userAccountAddress)} MINA`);
+console.log(
+  `Harpo Account Balance: ${Mina.getBalance(harpoFeePayerAccount)} MINA`
+);
+console.log(`initial balance: ${zkapp.account.balance.get().div(1e9)} MINA`);
+console.log(`Nullifier ` + nullifier);
+console.log(`Commitment  ` + commitment);
+
 /**
  * Function to create  the Commitment C(0) = H(S(0),N(0))
  */
-// async function createCommitment(nullifier: any) {
-//   let secret = Field.random();
-//   let commitment = Poseidon.hash([nullifier, secret]);
-//   return commitment;
-// }
+async function createCommitment(nullifier: any) {
+  let secret = Field.random();
+  let commitment = Poseidon.hash([nullifier, secret]);
+  return commitment;
+}
 
-/**
- *Merkle Tree implementation 
- 1. Create Merkle Tree witness 
- 2. Wrap  the Merkle Tree into a off-chain storage form 
- */
-//Creating the Merkle wiotness
-
-class MerkleWitness extends Experimental.MerkleWitness(8) {}
-const mixerTree = new Experimental.MerkleTree(8);
+// Inserting a commitment in the Merkle Tree
+console.log('-------------Inserting a commitment----------------------');
+console.log(
+  'Merkle tree root (pre insertion)',
+  zkapp.merkleTreeRoot.get().toString()
+);
+console.log('-->', merkleTree.getRoot().toString());
+zkapp.insertCommitment(commitment);
+console.log('-->', merkleTree.getRoot().toString());
+console.log(
+  'Merkle tree root (pos insertion)',
+  zkapp.merkleTreeRoot.get().toString()
+);
 
 //We will set a experimental commitment to our Merkle Tree
 let testHash = Poseidon.hash([Field.random()]);
 
-//Setting testing Leafs into the Tree
-// mixerTree.setLeaf(1n,testHash);
-//Creating a initial commitment
-let initialCommitment = mixerTree.getRoot();
-console.log('Initial Commitment' + initialCommitment);
+console.log('update');
+tx = await Mina.transaction(harpoFeePayer, () => {
+  zkapp.update(new Field(3));
+  if (!doProofs) zkapp.sign(zkappKey);
+});
+if (doProofs) await tx.prove();
+await tx.send();
 
-// console.log('update');
-// tx = await Mina.transaction(harpoFeePayer, () => {
-//   zkapp.update(new Field(3));
-//   if (!doProofs) zkapp.sign(zkappKey);
-// });
-// if (doProofs) await tx.prove();
-// await tx.send();
-
-// // pay more into the zkapp -- this doesn't need a proof
+// pay more into the zkapp -- this doesn't need a proof
 // console.log('receive');
 // tx = await Mina.transaction(harpoFeePayer, () => {
 //   let payerAccountUpdate = AccountUpdate.createSigned(harpoFeePayer);
@@ -228,11 +261,8 @@ console.log('Initial Commitment' + initialCommitment);
 // });
 // await tx.send();
 
-// console.log('payout');
-
 // tx = await Mina.transaction(harpoFeePayer, () => {
 //   AccountUpdate.fundNewAccount(harpoFeePayer);
-//   zkapp.payout(privilegedKey);
 //   if (!doProofs) zkapp.sign(zkappKey);
 // });
 // if (doProofs) await tx.prove();
@@ -243,22 +273,9 @@ console.log('Initial Commitment' + initialCommitment);
 
 // console.log('try to payout a second time..');
 // tx = await Mina.transaction(harpoFeePayer, () => {
-//   zkapp.payout(privilegedKey);
 //   if (!doProofs) zkapp.sign(zkappKey);
 // });
 // try {
-//   if (doProofs) await tx.prove();
-//   await tx.send();
-// } catch (err: any) {
-//   console.log('Transaction failed with error', err.message);
-// }
-
-// console.log('try to payout to a different account..');
-// try {
-//   tx = await Mina.transaction(harpoFeePayer, () => {
-//     zkapp.payout(Local.testAccounts[2].privateKey);
-//     if (!doProofs) zkapp.sign(zkappKey);
-//   });
 //   if (doProofs) await tx.prove();
 //   await tx.send();
 // } catch (err: any) {
